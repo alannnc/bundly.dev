3bdb06c1de6bff6e0440cef34125d0fb6dd2aaa9
Alan <alannnc@gmail.com>
2024-02-10 23:07:30 -0700

    update logos and slogans

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
-import HeroImage from '@/public/images/hero-image.png'
+import HeroImage from "@/public/images/hero-image.png";
 
 export default function Hero() {
   return (
@@ -11,10 +11,15 @@ export default function Hero() {
           {/* Hero content */}
           <div className="max-w-3xl mx-auto text-center">
             <h1 className="h1 font-hkgrotesk mb-6" data-aos="fade-up">
-              A powerful suite of user-centric products
+              Building awesome web and mobile products.
             </h1>
-            <p className="text-xl text-slate-500 mb-10" data-aos="fade-up" data-aos-delay="100">
-              Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+            <p
+              className="text-xl text-slate-500 mb-10"
+              data-aos="fade-up"
+              data-aos-delay="100"
+            >
+              We code, design and launch products that are easy to use, fast,
+              and can scale in the new Web era.
             </p>
             <div
               className="max-w-xs mx-auto sm:max-w-none sm:inline-flex sm:justify-center space-y-4 sm:space-y-0 sm:space-x-4"
@@ -22,26 +27,42 @@ export default function Hero() {
               data-aos-delay="200"
             >
               <div>
-                <Link className="btn text-white bg-indigo-500 hover:bg-indigo-600 w-full shadow-sm group" href="/signup">
-                  Get Started Free{' '}
+                <Link
+                  className="btn text-white bg-indigo-500 hover:bg-indigo-600 w-full shadow-sm group"
+                  href="/signup"
+                >
+                  Contact us{" "}
                   <span className="tracking-normal text-sky-300 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">
                     -&gt;
                   </span>
                 </Link>
               </div>
               <div>
-                <a className="btn text-slate-300 bg-slate-700 hover:bg-slate-600 border-slate-600 w-full shadow-sm" href="#0">
-                  Read Docs
+                <a
+                  className="btn text-slate-300 bg-slate-700 hover:bg-slate-600 border-slate-600 w-full shadow-sm"
+                  href="#0"
+                >
+                  Portfolio
                 </a>
               </div>
             </div>
           </div>
           {/* Hero image */}
-          <div className="pt-16 md:pt-20" data-aos="fade-up" data-aos-delay="300">
-            <Image className="mx-auto" src={HeroImage} width={920} height={518} alt="Hero" />
+          <div
+            className="pt-16 md:pt-20"
+            data-aos="fade-up"
+            data-aos-delay="300"
+          >
+            <Image
+              className="mx-auto"
+              src={HeroImage}
+              width={920}
+              height={518}
+              alt="Hero"
+            />
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Footer() {
   return (
@@ -11,13 +11,29 @@ export default function Footer() {
             <div className="mb-2">
               {/* Logo */}
               <Link className="inline-flex" href="/" aria-label="Cruip">
-                <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                <svg
+                  className="w-8 h-8"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <defs>
-                    <linearGradient x1="0%" y1="32.443%" x2="104.18%" y2="50%" id="flogo-a">
+                    <linearGradient
+                      x1="0%"
+                      y1="32.443%"
+                      x2="104.18%"
+                      y2="50%"
+                      id="flogo-a"
+                    >
                       <stop stopColor="#FFF" stopOpacity=".299" offset="0%" />
                       <stop stopColor="#7587E4" stopOpacity="0" offset="100%" />
                     </linearGradient>
-                    <linearGradient x1="18.591%" y1="0%" x2="100%" y2="100%" id="flogo-b">
+                    <linearGradient
+                      x1="18.591%"
+                      y1="0%"
+                      x2="100%"
+                      y2="100%"
+                      id="flogo-b"
+                    >
                       <stop stopColor="#818CF8" offset="0%" />
                       <stop stopColor="#C7D2FE" offset="100%" />
                     </linearGradient>
@@ -25,8 +41,15 @@ export default function Footer() {
                   <g fill="none" fillRule="evenodd">
                     <path fill="#3730A3" d="M16 18.5V32l15.999-9.25V9.25z" />
                     <path fill="#4F46E5" d="m0 23 16 9V18.501L0 9.251z" />
-                    <path fillOpacity=".64" fill="url(#flogo-a)" d="M16 13 0 23l16 9 16-9z" />
-                    <path fill="url(#flogo-b)" d="M16 0 0 9.25l16 9.25 15.999-9.25z" />
+                    <path
+                      fillOpacity=".64"
+                      fill="url(#flogo-a)"
+                      d="M16 13 0 23l16 9 16-9z"
+                    />
+                    <path
+                      fill="url(#flogo-b)"
+                      d="M16 0 0 9.25l16 9.25 15.999-9.25z"
+                    />
                   </g>
                 </svg>
               </Link>
@@ -34,25 +57,39 @@ export default function Footer() {
           </div>
           {/* 2nd block */}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-xs text-slate-200 font-semibold uppercase underline mb-3">Products</h6>
+            <h6 className="text-xs text-slate-200 font-semibold uppercase underline mb-3">
+              Products
+            </h6>
             <ul className="text-sm space-y-2">
               <li>
-                <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="#0">
-                  Cube Manage
+                <a
+                  className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
+                  href="#0"
+                >
+                  IC-CONNECT-JS
                 </a>
               </li>
               <li>
-                <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="#0">
-                  Cube Analyse
+                <a
+                  className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
+                  href="#0"
+                >
+                  Calendsync.com
                 </a>
               </li>
               <li>
-                <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="#0">
-                  Cube Launch
+                <a
+                  className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
+                  href="#0"
+                >
+                  Bundly Launch
                 </a>
               </li>
               <li>
-                <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="#0">
+                <a
+                  className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
+                  href="#0"
+                >
                   Experimentation
                 </a>
               </li>
@@ -60,25 +97,39 @@ export default function Footer() {
           </div>
           {/* 3rd block */}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-xs text-slate-200 font-semibold uppercase underline mb-3">Resources</h6>
+            <h6 className="text-xs text-slate-200 font-semibold uppercase underline mb-3">
+              Resources
+            </h6>
             <ul className="text-sm space-y-2">
               <li>
-                <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="#0">
+                <a
+                  className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
+                  href="#0"
+                >
                   Blog
                 </a>
               </li>
               <li>
-                <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="#0">
+                <a
+                  className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
+                  href="#0"
+                >
                   Cheat Sheet
                 </a>
               </li>
               <li>
-                <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="#0">
+                <a
+                  className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
+                  href="#0"
+                >
                   Channel Partners
                 </a>
               </li>
               <li>
-                <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="#0">
+                <a
+                  className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
+                  href="#0"
+                >
                   Affiliate Program
                 </a>
               </li>
@@ -86,25 +137,39 @@ export default function Footer() {
           </div>
           {/* 4th block */}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-xs text-slate-200 font-semibold uppercase underline mb-3">Projects</h6>
+            <h6 className="text-xs text-slate-200 font-semibold uppercase underline mb-3">
+              Projects
+            </h6>
             <ul className="text-sm space-y-2">
               <li>
-                <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="#0">
+                <a
+                  className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
+                  href="#0"
+                >
                   Session Recording
                 </a>
               </li>
               <li>
-                <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="#0">
+                <a
+                  className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
+                  href="#0"
+                >
                   Feature Flags
                 </a>
               </li>
               <li>
-                <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="#0">
+                <a
+                  className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
+                  href="#0"
+                >
                   Heatmaps
                 </a>
               </li>
               <li>
-                <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="#0">
+                <a
+                  className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
+                  href="#0"
+                >
                   Correlation Analysis
                 </a>
               </li>
@@ -112,20 +177,31 @@ export default function Footer() {
           </div>
           {/* 5th block */}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-xs text-slate-200 font-semibold uppercase underline mb-3">Company</h6>
+            <h6 className="text-xs text-slate-200 font-semibold uppercase underline mb-3">
+              Company
+            </h6>
             <ul className="text-sm space-y-2">
               <li>
-                <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="#0">
+                <a
+                  className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
+                  href="#0"
+                >
                   About Us
                 </a>
               </li>
               <li>
-                <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="#0">
+                <a
+                  className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
+                  href="#0"
+                >
                   Our Story
                 </a>
               </li>
               <li>
-                <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="#0">
+                <a
+                  className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
+                  href="#0"
+                >
                   Work With Us
                 </a>
               </li>
@@ -139,10 +215,14 @@ export default function Footer() {
             <li>
               <a
                 className="flex justify-center items-center text-indigo-500 hover:text-slate-300 transition duration-150 ease-in-out"
-                href="#0"
-                aria-label="Twitter"
+                href="https://x.com/bundlydev"
+                aria-label="X"
               >
-                <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                <svg
+                  className="w-8 h-8 fill-current"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <path d="m13.063 9 3.495 4.475L20.601 9h2.454l-5.359 5.931L24 23h-4.938l-3.866-4.893L10.771 23H8.316l5.735-6.342L8 9h5.063Zm-.74 1.347h-1.457l8.875 11.232h1.36l-8.778-11.232Z" />
                 </svg>
               </a>
@@ -153,7 +233,11 @@ export default function Footer() {
                 href="#0"
                 aria-label="Medium"
               >
-                <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                <svg
+                  className="w-8 h-8 fill-current"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <path d="M23 8H9a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1Zm-1.708 3.791-.858.823a.251.251 0 0 0-.1.241V18.9a.251.251 0 0 0 .1.241l.838.823v.181h-4.215v-.181l.868-.843c.085-.085.085-.11.085-.241v-4.887l-2.41 6.131h-.329l-2.81-6.13V18.1a.567.567 0 0 0 .156.472l1.129 1.37v.181h-3.2v-.181l1.129-1.37a.547.547 0 0 0 .146-.472v-4.749a.416.416 0 0 0-.138-.351l-1-1.209v-.181H13.8l2.4 5.283 2.122-5.283h2.971l-.001.181Z" />
                 </svg>
               </a>
@@ -161,10 +245,14 @@ export default function Footer() {
             <li className="ml-2">
               <a
                 className="flex justify-center items-center text-indigo-500 hover:text-slate-300 transition duration-150 ease-in-out"
-                href="#0"
+                href="https://github.com/bundlydev"
                 aria-label="Github"
               >
-                <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                <svg
+                  className="w-8 h-8 fill-current"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
                 </svg>
               </a>
@@ -172,10 +260,22 @@ export default function Footer() {
           </ul>
           {/* Links */}
           <div className="text-sm text-slate-600">
-            <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="#0">Terms</a> · <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="#0">Privacy Policy</a>
+            <a
+              className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
+              href="#0"
+            >
+              Terms
+            </a>{" "}
+            ·{" "}
+            <a
+              className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
+              href="#0"
+            >
+              Privacy Policy
+            </a>
           </div>
         </div>
       </div>
     </footer>
-  )
+  );
 }
